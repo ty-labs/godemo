75561ae4aedf2d14850fa263223c13071b8bb6f3
tylowe-labs <tylerlowe@gmail.com>
2023-11-18 19:26:08 -0800

    godemo: changes for v1.0.0

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tylowe-labs/godemo
+
+go 1.21.3
---
+++ godemo.go
@@ -0,0 +1,58 @@
+package godemo
+
+type OrderableFn[T any] func(t1, t2 T) int
+
+type Tree[T any] struct {
+	Root    *node[T]
+	OrderFn OrderableFn[T]
+}
+type node[T any] struct {
+	val   T
+	left  *node[T]
+	right *node[T]
+}
+
+func (t *Tree[T]) Insert(val T) {
+	t.Root = t.Root.insert(val, t.OrderFn)
+}
+
+func (t *Tree[T]) Contains(val T) bool {
+	return t.Root.contains(val, t.OrderFn)
+}
+
+func (n *node[T]) insert(val T, f OrderableFn[T]) *node[T] {
+	if n == nil {
+		return &node[T]{
+			val: val,
+		}
+	}
+	switch order := f(n.val, val); order {
+	case -1:
+		n.left = n.left.insert(val, f)
+	case 1:
+		n.right = n.right.insert(val, f)
+	default:
+		n.val = val
+	}
+	return n
+}
+
+func (n *node[T]) contains(val T, f OrderableFn[T]) bool {
+	if n == nil {
+		return false
+	}
+	switch order := f(n.val, val); order {
+	case -1:
+		return n.left.contains(val, f)
+	case 1:
+		return n.right.contains(val, f)
+	default:
+		return true
+	}
+}
+
+func NewTree[T any](f OrderableFn[T]) *Tree[T] {
+	return &Tree[T]{
+		OrderFn: f,
+	}
+}
